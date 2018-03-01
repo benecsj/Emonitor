ba444e1f9b7c6c2a7bbcd4b8922242bc1943020b
jooo <benecsj@gmail.com>
2018-03-01 12:51:47 +0100

    minor fix

--- fs.sh
+++ fs.sh
@@ -1,1 +1,1 @@
-mkspiffs -d 5 -b 4096 -p 128 -s 262144 -c /home/jooo/ESP8266_RTOS_SDK/examples/Emonitor/data/ /home/jooo/ESP8266_RTOS_SDK/bin/spiffs-image.bin
+mkspiffs -d 5 -b 4096 -p 128 -s 262144 -c /home/jooo/ESP8266_RTOS_SDK/projects/Emonitor/data/ /home/jooo/ESP8266_RTOS_SDK/bin/spiffs-image.bin
